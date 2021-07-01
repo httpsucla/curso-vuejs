783261050a0e38c27409a6e929e842b516e7f175
httpsucla <paulasucla@hotmail.com>
2021-07-01 11:38:00 -0300

    resposta desafio 1

--- desafio-01/app.js
+++ desafio-01/app.js
@@ -2,6 +2,12 @@ new Vue ({
     el: '#desafio',
     data: {
         nome: 'Paula',
-        idade: '21 anos'
+        idade: 21,
+        image: "https://res.cloudinary.com/practicaldev/image/fetch/s--L71__yWO--/c_imagga_scale,f_auto,fl_progressive,h_900,q_auto,w_1600/https://dev-to-uploads.s3.amazonaws.com/i/z74oqos1984w5g5ah0i9.jpeg"
+    },
+    methods: {
+        random() {
+            return Math.random();
+        }
     }
 })
